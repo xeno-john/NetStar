23a1153d0fed08a6420a2c95844aad0e78184b67
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2020-11-16 21:36:39 +0200

    added registration page when creating an account, password is hashed and then stored in the database
    Signed-off-by: xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cx_Oracle
 from flask import Flask, render_template,request
 import os
+import bcrypt
 
 # we get our credentials from environment variables 
 db_user = str(os.environ.get('db_account_user'))
@@ -15,11 +16,27 @@ def verify_login(username,password):
     login_result = False
 
     for row in cursor.execute("select user_name,password from clients"):
-        if row[0]==username and row[1]==password:
+        if row[0]==username and bcrypt.checkpw(password.encode('utf-8'),row[1].encode('utf-8')):
             login_result = True
             break
 
     return login_result
+
+def verify_register(username,email,password):
+    ret_value = True
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    try:
+        command = str("insert into clients(user_name,e_mail,password,registration_date,account_credits) values('"+username+"','"+email+"','"+hashed_password.decode('utf-8')+"',SYSDATE,0)")
+        cursor.execute(command)
+        cursor.execute("commit work")
+    except cx_Oracle.DatabaseError as exc:
+        ret_value = False
+        error, = exc.args
+        print("Oracle-Error-Code:", error.code)
+        print("Oracle-Error-Message:", error.message)
+    
+    return ret_value
 
 server = Flask(__name__)
 
@@ -28,7 +45,7 @@ def index():
     return render_template('login.html')
 
 @server.route('/', methods=['POST'])
-def my_form_post():
+def base_get_login_data():
     username = request.form['username']
     password = request.form['password']
     # if we find the entered user_name and password in the database
@@ -36,6 +53,22 @@ def my_form_post():
         return render_template('index.html') # we move on to the index page
     else:
         return render_template('login.html') # otherwise we remain on the same page
+
+@server.route("/register", methods=['GET'])
+def render():
+    return render_template('register.html')
+
+@server.route("/register", methods=['POST'])
+def get_registerdata():
+    email = request.form['email']
+    username = request.form['username']
+    password = request.form['password']
+
+    if True == verify_register(username,email,password):
+        return "registration successful"
+    else:
+        return render_template('register.html')
+
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0', debug=True)
