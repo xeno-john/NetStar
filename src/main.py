2f1d342c847a65a3ef2ded4fc460de68fe247f97
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2020-11-21 18:53:41 +0200

    added initial profile page
    also, when logging in or registering, if credentials are incorrect, user is being warned in a graphical manner
    
    Signed-off-by: xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import cx_Oracle
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, redirect, url_for, session
 import os
 import bcrypt
+import logging
 
 # we get our credentials from environment variables 
 db_user = str(os.environ.get('db_account_user'))
@@ -12,6 +13,29 @@ db_connection = cx_Oracle.connect(db_user + '/' + db_password + '@//bd-dc.cs.tui
 # we create a cursor variable to execute commands
 cursor = db_connection.cursor()
 
+server = Flask(__name__)
+
+server.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@server.route("/")
+def base():
+    return redirect(url_for("login"))
+
+@server.route("/login")
+def login():
+    return render_template('login.html')
+
+@server.route('/login', methods=['POST'])
+def base_get_login_data():
+    username = request.form['username']
+    password = request.form['password']
+    # if we find the entered user_name and password in the database
+    if True == verify_login(username,password): 
+        session["username"] = username
+        return redirect(url_for("profile"))
+    else:
+        return render_template('login.html', error_message = True) # otherwise we remain on the same page
+
 def verify_login(username,password):
     login_result = False
 
@@ -21,38 +45,6 @@ def verify_login(username,password):
             break
 
     return login_result
-
-def verify_register(username,email,password):
-    ret_value = True
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-    try:
-        command = str("insert into clients(user_name,e_mail,password,registration_date,account_credits) values('"+username+"','"+email+"','"+hashed_password.decode('utf-8')+"',SYSDATE,0)")
-        cursor.execute(command)
-        cursor.execute("commit work")
-    except cx_Oracle.DatabaseError as exc:
-        ret_value = False
-        error, = exc.args
-        print("Oracle-Error-Code:", error.code)
-        print("Oracle-Error-Message:", error.message)
-    
-    return ret_value
-
-server = Flask(__name__)
-
-@server.route("/")
-def index():
-    return render_template('login.html')
-
-@server.route('/', methods=['POST'])
-def base_get_login_data():
-    username = request.form['username']
-    password = request.form['password']
-    # if we find the entered user_name and password in the database
-    if True == verify_login(username,password): 
-        return render_template('index.html') # we move on to the index page
-    else:
-        return render_template('login.html') # otherwise we remain on the same page
 
 @server.route("/register", methods=['GET'])
 def render():
@@ -65,10 +57,35 @@ def get_registerdata():
     password = request.form['password']
 
     if True == verify_register(username,email,password):
-        return "registration successful"
+        return redirect(url_for("login"))
     else:
-        return render_template('register.html')
+        return render_template('register.html', error_message = True)
 
+def verify_register(username,email,password):
+    ret_value = True
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    try:
+        command = str("insert into clients(user_name,e_mail,password,registration_date,account_credits) values('"+username+"','"+email+"','"+hashed_password.decode('utf-8')+"',SYSDATE,0)")
+        logging.info("Executed " + command)
+        cursor.execute(command)
+        cursor.execute("commit work")
+    except cx_Oracle.DatabaseError as exc:
+        ret_value = False
+        error, = exc.args
+        print("Oracle-Error-Code:", error.code)
+        print("Oracle-Error-Message:", error.message)
+    
+    return ret_value
+
+@server.route("/profile")
+def profile():
+    if "username" in session:
+        user_name = session["username"]
+        session.pop("username")
+        return render_template('profile.html', user_name = user_name)
+    else:
+        return redirect(url_for("base"))
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0', debug=True)
+   server.run(host='0.0.0.0', debug=True, threaded=True)
