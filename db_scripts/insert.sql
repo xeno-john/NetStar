850490aaa2f90c3ba52f2ac85a09af4253900285
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2020-12-31 03:36:58 +0200

    user can now make reservation
    also reservations are being checked for data overlapping
    
    Signed-off-by: xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>

--- db_scripts/insert.sql
+++ db_scripts/insert.sql
@@ -1,27 +1,112 @@
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Ionut','Baltariu','Manager','saltyjohn31@gmail.com','0743783791',8000,TO_DATE('2018/10/01 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Vlad','Paraschiv','Manager','vladparaschiv@gmail.com','0756718341',8000,TO_DATE('2018/10/01 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Bejenariu','Razvan','Database Operator','razvanbeje1@gmail.com','0749351369',4800,TO_DATE('2018/10/01 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Balta','Gabriel','Web - Back End','baltagaby@yahoo.com','0763493551',5120,TO_DATE('2018/10/01 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Florin','Citu','Operator PC','citufl@yahoo.com','0755828239',3800,TO_DATE('2018/10/01 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Ludovic','Bogdan','Operator PC','ludovic_bogdan@outlook.com','0756913512',3500,TO_DATE('2019/02/14 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Rares','Orban','Operator PC','r.orban@yahoo.com','0743915290',3500,TO_DATE('2019/02/14 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Luciano','Bote','Operator PC','luci.bote@gmail.com','0733793615',3300,TO_DATE('2020/03/15 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+insert into ic_employees(first_name,last_name,role,e_mail,phone_number,salary,hiring_date)
+values('Raluca','Tzurcan','Operator PC','tz.raluca@outlook.com','0756679812',3300,TO_DATE('2020/03/15 09:00:00', 'YYYY/MM/DD HH:MI:SS'));
+
+
 insert into computer_configuration(CPU,GPU,RAM,MONITOR,MOUSE,KEYBOARD)
-values('Intel Core i9','Nvidia GeForce RTX 3070','32 gb DDR4','Samsung 4K Curbat','Razer Naga','Razer X');
+values('Intel Core i9','Nvidia GeForce RTX 3080 Ti','32 Gb DDR4 3200 MHz','Monitor LED Samsung 28" Curbat','Razer Naga Pro 3','Razer Huntsman');
+insert into computer_configuration(CPU,GPU,RAM,MONITOR,MOUSE,KEYBOARD)
+values('Amd Ryzen 5 3600','Nvidia GeForce RTX 2080 Ti','16 Gb DDR4 2666 MHz','Monitor LED Samsung 25" 1080p','Wireless Razer Mamba','Razer Black Widow');
+insert into computer_configuration(CPU,GPU,RAM,MONITOR,MOUSE,KEYBOARD)
+values('Amd Ryzen 5 2600X','Nvidia GeForce RTX 2060','16 Gb DDR4 2400 MHz','Monitor LED Samsung 25" 1080p','Logitech G502','Redragon K552B');
+
 insert into rooms(NAME,NUMBER_OF_COMPUTERS,CONFIGURATION_TYPE_FK)
 values('Gods Room','10',1);
+insert into rooms(NAME,NUMBER_OF_COMPUTERS,CONFIGURATION_TYPE_FK)
+values('Gladiators Room','12',2);
+insert into rooms(NAME,NUMBER_OF_COMPUTERS,CONFIGURATION_TYPE_FK)
+values('Pirates Room','15',3);
+
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
+values((select room_id_pk from rooms where name='Gods Room'),1);
 insert into computers(room_fk,computer_cfg_id_pk)
-values(1,1);
-insert 
-into appointments(computer_id,client_id,appointment_date,start_time,end_time,price)
-values(1,1,SYSDATE,TO_DATE('2020/12/31 09:30:00', 'yyyy/mm/dd hh24:mi:ss'),TO_DATE('2020/12/31 11:30:00', 'yyyy/mm/dd hh24:mi:ss'),6);
+values((select room_id_pk from rooms where name='Gods Room'),1);
+
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Gladiators Room'),2);
+
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+insert into computers(room_fk,computer_cfg_id_pk)
+values((select room_id_pk from rooms where name='Pirates Room'),3);
+
+commit work;
--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import os
 import bcrypt
 import logging
-from datetime import timedelta
+from datetime import datetime
 
 # we get our credentials from environment variables
 db_user = str(os.environ.get('db_account_user'))
 db_password = str(os.environ.get('db_account_pass'))
 
 # here we connect to the database
-db_connection = cx_Oracle.connect(db_user + '/' + db_password + '@//bd-dc.cs.tuiasi.ro:1539/orcl')
+db_connection = cx_Oracle.connect(db_user + '/' + db_password + '@//localhost:1521/xe')
 # we create a cursor variable to execute commands
 cursor = db_connection.cursor()
 
@@ -20,6 +20,7 @@ server = Flask(__name__)
 
 server.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+g_error_msg = ""
 
 @server.route("/")
 def base():
@@ -77,8 +78,8 @@ def verify_register(username, email, password):
 
     try:
         command = str(
-            "insert into clients(user_name,e_mail,password,registration_date,account_credits) values('" + username + "','" + email + "','" + hashed_password.decode(
-                'utf-8') + "',SYSDATE,0)")
+            "insert into clients(user_name,e_mail,password,registration_date,account_credits) values('"
+            + username + "','" + email + "','" + hashed_password.decode('utf-8') + "',SYSDATE,0)")
         logging.info("Executed " + command)
         cursor.execute(command)
         cursor.execute("commit work")
@@ -113,12 +114,52 @@ def profile():
 @server.route("/profile/reservation")
 def reservation():
     if "username" in session:
+        global g_error_msg
+        gods_1st = cursor.execute("select * from ("
+                                  " select computer_id_pk from computers a, computer_configuration b"
+                                  " where a.computer_cfg_id_pk = "
+                                  "( select configuration_type_fk from rooms where name = 'Gods Room'))"
+                                  " where rownum = 1").fetchall()[0][0]
+        gods_last = cursor.execute("select * from ("
+                                   " select computer_id_pk from computers a, computer_configuration b"
+                                   " where a.computer_cfg_id_pk = "
+                                   "( select configuration_type_fk from rooms where name = 'Gods Room')"
+                                   " order by computer_id_pk desc )"
+                                   " where rownum = 1").fetchall()[0][0]
+        gladiators_1st = cursor.execute("select * from ("
+                                        " select computer_id_pk from computers a, computer_configuration b"
+                                        " where a.computer_cfg_id_pk = "
+                                        "( select configuration_type_fk from rooms where name = 'Gladiators Room'))"
+                                        " where rownum = 1").fetchall()[0][0]
+        gladiators_last = cursor.execute("select * from ("
+                                         " select computer_id_pk from computers a, computer_configuration b"
+                                         " where a.computer_cfg_id_pk = "
+                                         "( select configuration_type_fk from rooms where name = 'Gladiators Room')"
+                                         " order by computer_id_pk desc )"
+                                         " where rownum = 1").fetchall()[0][0]
+        pirates_1st = cursor.execute("select * from ("
+                                     " select computer_id_pk from computers a, computer_configuration b"
+                                     " where a.computer_cfg_id_pk = "
+                                     "( select configuration_type_fk from rooms where name = 'Pirates Room'))"
+                                     " where rownum = 1").fetchall()[0][0]
+        pirates_last = cursor.execute("select * from ("
+                                      " select computer_id_pk from computers a, computer_configuration b"
+                                      " where a.computer_cfg_id_pk = "
+                                      "( select configuration_type_fk from rooms where name = 'Pirates Room')"
+                                      " order by computer_id_pk desc )"
+                                      " where rownum = 1").fetchall()[0][0]
         computers_list = []
         command = "select computer_id_pk from computers"
         for row in cursor.execute(command):
             computers_list.append(row[0])
 
-        return render_template("reservation.html", computers_list=computers_list)
+        error_msg = g_error_msg
+        g_error_msg = ""
+        print(error_msg)
+
+        return render_template("reservation.html", computers_list=computers_list, gods_1st=gods_1st,
+                               gods_last=gods_last, gladiators_1st=gladiators_1st, gladiators_last=gladiators_last,
+                               pirates_1st=pirates_1st, pirates_last=pirates_last, error_msg=error_msg)
     else:
         return redirect(url_for("login"))
 
@@ -126,17 +167,38 @@ def reservation():
 @server.route("/profile/reservation", methods=['POST'])
 def check_reservation():
     if "username" in session:
-        user_name = session["username"]
+        global g_error_msg
+        sysdate = datetime.now().strftime("%d/%m/%Y")
         selected_computer = request.form['computer']
-        beginning_hour = request.form['beginning_hour']
-        ending_hour = request.form['ending_hour']
+        beginning_hour_raw = sysdate + ' ' + request.form['beginning_hour']
+        ending_hour_raw = sysdate + ' ' + request.form['ending_hour']
 
-        # TO DO, insert reservation in database
-        # 1st I must find a way to avoid overlapping
+        beginning_hour = datetime.strptime(beginning_hour_raw, "%d/%m/%Y %H:%M")
+        ending_hour = datetime.strptime(ending_hour_raw, "%d/%m/%Y %H:%M")
 
-        # user_id_pk = \
-        #    cursor.execute("select user_id_pk from clients where user_name = '" + user_name + "'").fetchall()[0][0]
-        return "tbd"
+        if beginning_hour > ending_hour:
+            g_error_msg = "Beginning hour is later than ending hour."
+            return redirect(url_for("reservation"))
+        elif not (beginning_hour.minute == 0 or beginning_hour.minute == 30):
+            g_error_msg = "Beginning hour must be either o'clock or half."
+            return redirect(url_for("reservation"))
+        elif not (ending_hour.minute == 0 or ending_hour.minute == 30):
+            g_error_msg = "Ending hour must be either o'clock or half."
+            return redirect(url_for("reservation"))
+
+        for row in cursor.execute("select start_time,end_time,computer_id from appointments"):
+            if int(selected_computer) == int(row[2]):
+                if beginning_hour <= row[1] and ending_hour >= row[0]:
+                    g_error_msg = "Reservation for the given computer overlaps with another reservation."
+                    return redirect(url_for("reservation"))
+        cursor.execute("insert into appointments(computer_id,client_id,appointment_date,start_time,end_time,price) " +
+                       "values(" + selected_computer + ",(select user_id_pk from clients where user_name = '" +
+                       session["username"] + "')," +
+                       "to_date('" + str(beginning_hour) + "','YYYY/MM/DD hh24:MI:SS'),to_date('" +
+                       str(beginning_hour) + "','YYYY/MM/DD hh24:MI:SS'),to_date('" + str(ending_hour) +
+                       "','YYYY/MM/DD hh24:MI:SS'),6)")
+        cursor.execute("commit work")
+        return redirect(url_for("profile"))
 
 
 @server.route("/profile/account", methods=['GET'])
@@ -183,10 +245,12 @@ def username_page_post():
             cursor.execute("select user_name from clients where user_id_pk = " + str(user_id_pk)).fetchall()[0][0]
         if current_username != username_from_db:
             return render_template("username.html", error_message="Current username is not correct.")
-        if new_username != new_username_confirmation:
+        elif new_username != new_username_confirmation:
             return render_template("username.html", error_message="Please make sure that you confirmed correctly the "
                                                                   "new username.")
-        if current_username == username_from_db and new_username == new_username_confirmation:
+        elif current_username == new_username:
+            return render_template("username.html", error_message="New name can't be the same as the old one.")
+        else:
             cursor.execute(
                 "update clients set user_name = '" + new_username + "' where user_id_pk = " + str(user_id_pk))
             session["username"] = new_username
@@ -219,10 +283,12 @@ def email_page_post():
                 0]
         if current_email != email_from_db:
             return render_template("email.html", error_message="Current email is not correct.")
-        if new_email != new_email_confirmation:
+        elif new_email != new_email_confirmation:
             return render_template("email.html", error_message="Please make sure that you confirmed correctly the "
                                                                "new email.")
-        if current_email == email_from_db and new_email == new_email_confirmation:
+        elif current_email == new_email:
+            return render_template("email.html", error_message="New email can't be the same as the old one..")
+        else:
             cursor.execute(
                 "update clients set e_mail = '" + new_email + "' where user_id_pk = " + str(user_id_pk))
             cursor.execute("commit work")
@@ -254,18 +320,15 @@ def password_page_post():
                 0]
         if not bcrypt.checkpw(current_password.encode('utf-8'), password_from_db.encode('utf-8')):
             return render_template("password.html", error_message="Incorrect password.")
-        if new_password != new_password_confirmation:
+        elif new_password != new_password_confirmation:
             return render_template("password.html", error_message="Please make sure that you confirmed correctly the "
                                                                   "new password.")
-
-        if bcrypt.checkpw(current_password.encode('utf-8'),
-                          password_from_db.encode('utf-8')) and new_password == new_password_confirmation:
+        else:
             cursor.execute("update clients set password = '" + bcrypt.hashpw(new_password.encode('utf-8'),
                                                                              bcrypt.gensalt()).decode(
                 'utf-8') + "' where user_id_pk = " + str(
                 user_id_pk))
             cursor.execute("commit work")
-            return redirect(url_for("login"))
         return redirect(url_for("login"))
 
 
